6fb1c281eed86fea2914907096a7daf95064226e
eloi.galan <eloi.galan@cceupma13p1.eupm>
2018-05-07 11:47:34 +0200

    afegida capa 3 sense testejarho

--- DTMF_P2_V0_restored/capa3.vhd
+++ DTMF_P2_V0_restored/capa3.vhd
@@ -8,55 +8,36 @@ entity capa3 is
 port (clk    : in  std_logic;
       clk_en : in  std_logic;
 		simbol : in std_logic_vector(4 downto 0 );
+		bc0,bc1,bc2,bc3 : out std_logic;
 		enable3M: out std_logic
      );
 end entity capa3;
 
 architecture arq of capa3 is
-	signal freq : std_logic_vector(7 downto 0);
-	signal enb : std_logic;
-
-begin 
-	freq(0) <= l0;
-	freq(1) <= l1;
-	freq(2) <= l2;
-	freq(3) <= l3;
-	
-	freq(4) <= h0;
-	freq(5) <= h1;
-	freq(6) <= h2;
-	freq(7) <= h3;
-	
+	signal estat : std_logic := '0';
+	signal caracter : std_logic_vector(3 downto 0):= "0000";
+begin 	
 		   process(clk)
            begin
 			  if rising_edge(clk) and clk_en='1' then
-					enable23 <= '0';
-					enb <= '1';
-					case freq is
-						when "10000100" =>   s_c2 <= "00000"; --0
-						when "00010001" =>   s_c2 <= "00001"; --1
-						when "00010100" =>   s_c2 <= "00010";	--2
-						when "00010010" =>   s_c2 <= "00011";	--3	
-						when "00100001" =>   s_c2 <= "00100"; --4
-						when "00100100" =>   s_c2 <= "00101"; --5
-						when "00100010" =>   s_c2 <= "00110"; --6
-						when "01000001" =>   s_c2 <= "00111"; --7
-						when "01000100" =>   s_c2 <= "01000"; --8
-						when "01000010" =>   s_c2 <= "01001"; --9
-						
-						when "00011000" =>   s_c2 <= "01010"; --A
-						when "00101000" =>   s_c2 <= "01011"; --B
-						when "01001000" =>   s_c2 <= "01100"; --C
-						when "10001000" =>   s_c2 <= "01101"; --D
-						
-						when "10000001" =>   s_c2 <= "01110"; --*
-						when "10000010" =>   s_c2 <= "01111"; --#
-						
-						when "00000000" =>   s_c2 <= "10000";--Silenci
-						when others => enb <= '0';
+					enable3M <= '0';
+					case estat is
+						when '0' =>
+							if simbol /= "10000"	then
+								caracter <= simbol(3 downto 0);
+								enable3M <= '1';
+								estat <= '1';
+							end if;
+						when '1' =>
+							if simbol = "10000"	then
+								estat <= '0';
+							end if;
 					end case;
-					enable23 <= enb;
 				end if;
+				bc0<=caracter(0);
+				bc1<=caracter(1);
+				bc2<=caracter(2);
+				bc3<=caracter(3);
 		   end process;		
 
 end architecture arq;      
